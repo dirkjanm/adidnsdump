8bbb4b05b2d1b792f3c77ce0a4a762ab9e08727d
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2023-12-13 16:56:43 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='adidnsdump',
-      version='1.3.0',
+      version='1.3.1',
       description='Active Directory Integrated DNS dumping by any authenticated user',
       license='MIT',
       classifiers=[
@@ -15,7 +15,7 @@ setup(name='adidnsdump',
           'Programming Language :: Python :: 3.7',
       ],
       author='Dirk-jan Mollema (@_dirkjan)',
-      author_email='dirkjan.mollema@fox-it.com',
+      author_email='dirkjan@dirkjanm.io',
       url='https://github.com/dirkjanm/adidnsdump',
       packages=['adidnsdump'],
       install_requires=['impacket', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6'],
